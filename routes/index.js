352ec228fb6799367a6aabc838e34bb8ebf2011e
IbeVdV <ibevandeveire@gmail.com>
2021-03-27 11:49:00 +0100

    added basic navbar met json feed

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,36 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { 
+    title: 'Takken',
+    takken: {
+      kapoenen: {
+        naam: 'Kapoenen'
+      },
+      kabouters: {
+        naam: 'Kabouters'
+      },
+      welpen: {
+        naam: 'Welpen'
+      },
+      jonggidsen: {
+        naam: 'Jonggidsen,'
+      },
+      jongverkenners: {
+        naam: 'Jongverkenners'
+      },
+      givers: {
+        naam: 'Givers'
+      },
+      jin: {
+        naam: 'Jin'
+      },
+      groepsleiding: {
+        naam: 'Groepsleiding'
+      },
+
+    } 
+  });
 });
 
 module.exports = router;
