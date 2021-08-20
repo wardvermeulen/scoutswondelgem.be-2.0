af1b514ef03d506cceb3854d20e66bdc72a830da
wardvermeulen <wardvermeulen@telenet.be>
2021-08-20 11:46:06 +0200

    mobile navbar

--- views/includes/navbar.jsx
+++ views/includes/navbar.jsx
@@ -76,8 +76,8 @@ function NavItemMobile(props) {
     return (
       <div className="dropdown relative">
         <div>
-          <button className="dropdown-button inline-flex items-center justify-center w-full rounded-md px-3 py-2 text-gray-700 text-sm hover:bg-lime-300 hover:shadow focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-lime-500">
-            {props.naam}
+          <button className="dropdown-button inline-flex justify-between items-center w-full rounded-md px-3 py-2 text-gray-700 text-sm hover:bg-gray-300 hover:shadow focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-gray-500">
+            {props.rechts ? "" : props.naam}
             <svg
               className="-mr-1 ml-2 h-5 w-5"
               xmlns="http://www.w3.org/2000/svg"
@@ -91,13 +91,14 @@ function NavItemMobile(props) {
                 clipRule="evenodd"
               />
             </svg>
+            {props.rechts ? props.naam : ""}
           </button>
         </div>
 
         <div
           className={
-            (props.rechts ? "origin-top-right right-0 " : "origin-top-left left-0 ") +
-            "dropdown-menu absolute mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none hidden"
+            (props.rechts ? "origin-top-left left-0 " : "origin-top-right right-0 ") +
+            "dropdown-menu absolute mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none hidden z-10"
           }
         >
           <div className="py-1">{items}</div>
@@ -108,7 +109,10 @@ function NavItemMobile(props) {
     return (
       <a
         href={"/" + props.tabel_en_url_naam}
-        className="w-full rounded-md px-3 py-2 text-gray-700 text-sm hover:bg-lime-300 hover:shadow focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-lime-500"
+        className={
+          (props.rechts ? "text-right " : "") +
+          "block rounded-md px-3 py-2 text-gray-700 text-sm hover:bg-gray-300 hover:shadow focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-gray-500"
+        }
       >
         {props.naam}
       </a>
@@ -117,7 +121,7 @@ function NavItemMobile(props) {
 }
 
 function Navbar(props) {
-  let navLinks, navRechts;
+  let navLinks, navRechts, navMobileLinks, navMobileRechts;
   if (props.navbar) {
     navLinks = props.navbar.map(function (value, index) {
       if (value.rechts === false) {
@@ -146,14 +150,44 @@ function Navbar(props) {
         );
       }
     });
+
+    navMobileLinks = props.navbar.map(function (value, index) {
+      if (value.rechts === false) {
+        return (
+          <NavItemMobile
+            key={index}
+            naam={value.naam}
+            dropdown_items={value.dropdown_items}
+            tabel_en_url_naam={value.tabel_en_url_naam}
+            rechts={false}
+          ></NavItemMobile>
+        );
+      }
+    });
+
+    navMobileRechts = props.navbar.map(function (value, index) {
+      if (value.rechts === true) {
+        return (
+          <NavItemMobile
+            key={index}
+            naam={value.naam}
+            dropdown_items={value.dropdown_items}
+            tabel_en_url_naam={value.tabel_en_url_naam}
+            rechts={true}
+          ></NavItemMobile>
+        );
+      }
+    });
   }
 
   return (
     <nav className="p-2">
+      {/* Desktop navbar */}
       <div className="max-w-6xl mx-auto bg-lime-100 px-4 rounded-lg shadow">
         <div className="flex justify-between h-16">
           <div className="flex space-x-10">
             <div className="flex items-center space-x-2">
+              {/* Logo */}
               <a
                 href="#"
                 className="w-full rounded-md px-3 py-2 text-gray-700 font-bold bg-lime-200 hover:bg-lime-300 hover:shadow focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-lime-500"
@@ -161,9 +195,14 @@ function Navbar(props) {
                 Scouts Wondelgem
               </a>
             </div>
+            {/* Linkse navigatie items */}
             <div className="hidden lg:flex items-center space-x-2">{navLinks}</div>
           </div>
+
+          {/* Rechtse navigatie items (zoals bv. Aanmelden) */}
           <div className="hidden lg:flex items-center space-x-2">{navRechts}</div>
+
+          {/* Open en gesloten menu knop */}
           <div className="lg:hidden flex items-center">
             <button
               id="mobile-menu-button"
@@ -194,13 +233,10 @@ function Navbar(props) {
         </div>
       </div>
 
-      <div id="mobile-menu" className="hidden lg:hidden bg-gray-100 rounded-lg shadow mt-2 mx-2">
-        <a href="" className="block py-2 px-4 text-sm hover:bg-gray-200 rounded-t-lg">
-          Takken
-        </a>
-        <a href="" className="block py-2 px-4 text-sm hover:bg-gray-200">
-          Informatie
-        </a>
+      {/* Mobiele navbar */}
+      <div id="mobile-menu" className="flex justify-between items-center lg:hidden mt-2">
+        <div className="bg-gray-100 rounded-lg shadow mx-2 w-full">{navMobileLinks}</div>
+        <div className="bg-gray-100 rounded-lg shadow mx-2 w-full">{navMobileRechts}</div>
       </div>
     </nav>
   );
